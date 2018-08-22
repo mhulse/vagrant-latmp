8b113c7d2e39c643df147cb2e8e1c07a28ca0672
Micky Hulse <mickyhulse@gmail.com>
2018-08-21 20:46:22 -0700

    Added license

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ function install() {
   # Remote repo zip file:
   SOURCE_ZIP="https://github.com/mhulse/vagrant-latmp/tarball/master"
   # Get the zip file and extract all files:
-  curl -sS -#L "$SOURCE_ZIP" | tar -xzv --strip-components 1 --exclude={install.sh,.*,README*}
+  curl -sS -#L "$SOURCE_ZIP" | tar -xzv --strip-components 1 --exclude={install.sh,.*,README*,LICENSE}
 
   # Testing (comment out the above and run these lines instead):
   #SOURCE_ZIP="/Users/mhulse/Desktop/test.tar.gz"
